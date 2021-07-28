b88365ff2699449537092cd93c7472b3a73738ee
Romulo <romulojusto@gmail.com>
2021-07-28 16:45:20 -0400

    Updated models and routes folders

---
+++ routes/api/category-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  try{
+    const connection = await Category.findAll({
+      include: [Product]
+    })
+    // with positive connection it reads the data given
+
+    res.status(200).json(connection)
+  }
+  catch(error){
+    // 500 internal server error
+    res.status(500).json(error)
+  }
+  // find all categories
+  // be sure to include its associated Products
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  try{
+    const connection = await Category.findByPk((req.params.id),{
+      include: [Product]
+    })
+    res.status(200).json(connection)
+  }
+  catch(error){
+    res.status(500).json(error)
+  }
+  // be sure to include its associated Products
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try{
+    const connection = await Category.create(req.body)
+    res.status(200).json(connection)
+  }catch(error){
+    res.status(500).json(error)
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try{
+    const connection = await Category.update((req.params.body), {
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(connection)
+  }catch(error){
+    res.status(404).json(error)
+  }
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
